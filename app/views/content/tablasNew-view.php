c41f984a58490fd95e65d0438b854a6612292480
Verónica Magali Quinde España <veronicaquinde@digitech.com.ec>
2024-06-21 12:33:59 -0500

    Actualizar el front end de la vista con search y corrección del collapse

--- app/views/content/tablasNew-view.php
+++ app/views/content/tablasNew-view.php
@@ -19,242 +19,176 @@
 	}	
 ?>
 
-<!DOCTYPE html>
+
 <html lang="es">
   <head>
     <meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo APP_NAME; ?> | Ingreso Tablas Catálogos</title>
+	<title><?php echo APP_NAME; ?>| Tablas</title>
 
 	<!-- Google Font: Source Sans Pro -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/fontawesome-free/css/all.min.css">
-	
-	<!-- daterange picker -->
-	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/daterangepicker/daterangepicker.css">
-	<!-- iCheck for checkboxes and radio inputs -->
-	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-	<!-- Bootstrap Color Picker -->
-	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
-	<!-- Tempusdominus Bootstrap 4 -->
-	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-	<!-- Select2 -->
-	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/select2/css/select2.min.css">
-	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-	<!-- Bootstrap4 Duallistbox -->
-	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
-	<!-- BS Stepper -->
-	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/bs-stepper/css/bs-stepper.min.css">
-	
+	<!-- DataTables -->
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/css/adminlte.min.css">
-
-
 	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/css/sweetalert2.min.css">
-	
-
-
+    
   </head>
   <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
 
-		<!-- Preloader -->
-		<!--?php require_once "app/views/inc/preloader.php"; ?-->
-		<!-- /.Preloader -->
+      <!-- Preloader -->
+      <!--?php require_once "app/views/inc/preloader.php"; ?-->
+      <!-- /.Preloader -->
 
-		<!-- Navbar -->
-		<?php require_once "app/views/inc/navbar.php"; ?>
-		<!-- /.navbar -->
+      <!-- Navbar -->
+      <?php require_once "app/views/inc/navbar.php"; ?>
+      <!-- /.navbar -->
 
-		<!-- Main Sidebar Container -->
-		<?php require_once "app/views/inc/main-sidebar.php"; ?>
-		<!-- /.Main Sidebar Container -->  
+      <!-- Main Sidebar Container -->
+      <?php require_once "app/views/inc/main-sidebar.php"; ?>
+      <!-- /.Main Sidebar Container -->  
 
-		<!-- vista -->
-		<div class="content-wrapper">
+      <!-- vista -->
+      <div class="content-wrapper">
 
-			<!-- Content Header (Page header) -->
-			<div class="content-header">
-				<div class="container-fluid">
-					<div class="row mb-2">
-						<div class="col-sm-6">
-							<h4 class="m-0">Ingreso Tablas</h4>
-						</div><!-- /.col -->
-						<div class="col-sm-6">
-							<ol class="breadcrumb float-sm-right">
-								<li class="breadcrumb-item"><a href="#">Inicio</a></li>
-								<li class="breadcrumb-item active">Ficha Alumno</li>
-							</ol>
-						</div><!-- /.col -->
-					</div><!-- /.row -->
-				</div><!-- /.container-fluid -->
-			</div>
-			<!-- /.content-header -->
+		<!-- Content Header (Page header) -->
+		<div class="content-header">
+			<div class="container-fluid">
+				<div class="row mb-2">
+					<div class="col-sm-6">
+						<h4 class="m-0">Ingreso Tablas</h4>
+					</div><!-- /.col -->
+					<div class="col-sm-6">
+						<ol class="breadcrumb float-sm-right">
+							<li class="breadcrumb-item"><a href="#">Inicio</a></li>
+							<li class="breadcrumb-item active"><a href="<?php echo APP_URL."dashboard/" ?>">Dashboard</a></li>
+						</ol>
+					</div><!-- /.col -->
+				</div><!-- /.row -->
+			</div><!-- /.container-fluid -->
+		</div>
+		<!-- /.content-header -->
 
-			<!-- Main content -->
-			<section class="content">				
-				<!-- /.container-fluid información alumno -->
-				<div class="container-fluid">
-
-					<div class="card card-default">						
-						<div class="card-header">
-							<h3 class="card-title">Tablas</h3>
-
-							<div class="card-tools">
+		<!-- Section listado de alumnos -->
+		<section class="content">	
+			<div class="container-fluid">
+				<div class="card card-default">
+					<div class="card-header" class="centered" >
+						<h4 class="card-title">Nueva tabla</h4>
+						<div class="card-tools">
 							<button type="button" class="btn btn-tool" data-card-widget="collapse">
 								<i class="fas fa-minus"></i>
 							</button>
-							<button type="button" class="btn btn-tool" data-card-widget="remove">
-								<i class="fas fa-times"></i>
-							</button>
-							</div>
 						</div>
+					</div>
 
-
-						<div class="card-body">
-							<div class="row">
-								<div class="col-md-12">
-
-								<form class="FormularioAjax" id="quickForm" action="<?php echo APP_URL; ?>app/ajax/tablasAjax.php" method="POST" autocomplete="off" enctype="multipart/form-data" >
-									<input type="hidden" name="modulo_tablas" value="<?php echo $modulo_tablas; ?>">
-									<input type="hidden" name="tabla_id" value="<?php echo $tablaid; ?>">											
-									
-									<div class="row">
-										<div class="col-md-8">
+					<div class="card-body">						
+						<div class="row">
+							<div class="col-md-12">	
+								<div class="row" class="col-md-3>			
+									<form class="FormularioAjax" id="quickForm" action="<?php echo APP_URL; ?>app/ajax/tablasAjax.php" method="POST" autocomplete="off" enctype="multipart/form-data" >
+										<input type="hidden" name="modulo_tablas" value="<?php echo $modulo_tablas; ?>">
+										<input type="hidden" name="tabla_id" value="<?php echo $tablaid; ?>">	
+										<div class="col-md-6">
 											<div class="form-group">
-												<label for="nombre_tabla">Nombre de la Tabla</label>
-												<input type="text" class="form-control" id="nombre_tabla" name="tabla_nombre" value="<?php echo $tabla_nombre; ?>">
+												<label for="tabla_nombre">Nombre</label>
+												<input type="text" class="form-control" id="tabla_nombre" name="tabla_nombre" value="<?php echo $tabla_nombre; ?>">
 											</div>
 										</div>
-
 										<div class="col-md-12">						
 											<button type="submit" class="btn btn-success btn-sm">Guardar</button>
 											<a href="<?php echo APP_URL; ?>tablasNew/" class="btn btn-info btn-sm">Cancelar</a>
 											<button type="reset" class="btn btn-dark btn-sm">Limpiar</button>						
-										</div>
-									</div>	
-								</form>
-
-									<div class="tab-custom-content">
-										<p class="lead mb-0">Tablas ingresadas</p>
-									</div>
-									<div class="tab-content" id="custom-content-above-tabContent">
-										<table id="example1" class="table table-bordered table-striped table-sm">
-											<thead>
-												<tr>
-													<th>Id</th>
-													<th>Nombre</th>
-													<th>Estado</th>															
-													<th style="width:300px;">Opciones</th>																
-												</tr>
-											</thead>
-											<tbody>
-												<?php 
-													echo $insTabla->listarTablas(); 
-												?>								
-											</tbody>
-										</table>
-									</div>
-
-								
-								
+										</div>									
+									</form>															
 								</div>
+								<div class="tab-custom-content">
+									<h4 class="card-title">Tablas ingresadas</h4>
+								</div>										
+								<div class="tab-content" id="custom-content-above-tabContent">							
+									<table id="example1" class="table table-bordered table-striped table-sm">
+										<thead>
+											<tr>
+												<th>Id</th>
+												<th>Nombre</th>
+												<th>Estado</th>															
+												<th	>Opciones</th>																
+											</tr>
+										</thead>
+										<tbody>
+											<?php 
+												echo $insTabla->listarTablas(); 
+											?>								
+										</tbody>
+									</table>									
+								</div>								
 							</div>
+						<!-- /.row -->
 						</div>
 					</div>
 				</div>
-			</section>
-			<!-- /.content -->
-      
-		</div>
-		<!-- /.vista -->
+			</div><!-- /.container-fluid -->
+		</section>
+		<!-- /.section -->      
+      </div>
+      <!-- /.vista -->
 
-		<?php require_once "app/views/inc/footer.php"; ?>
+      <?php require_once "app/views/inc/footer.php"; ?>
 
-		<!-- Control Sidebar -->
-		<aside class="control-sidebar control-sidebar-dark">
-		<!-- Control sidebar content goes here -->
-		</aside>
+      <!-- Control Sidebar -->
+      <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+      </aside>
       <!-- /.control-sidebar -->
     </div>
     <!-- ./wrapper -->
-
     
 	<!-- jQuery -->
 	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/jquery/jquery.min.js"></script>
 	<!-- Bootstrap 4 -->
 	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-	
-	<!-- Select2 -->
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/select2/js/select2.full.min.js"></script>
-	<!-- Bootstrap4 Duallistbox -->
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
-	<!-- InputMask -->
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/moment/moment.min.js"></script>
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/inputmask/jquery.inputmask.min.js"></script>
-	<!-- date-range-picker -->
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/daterangepicker/daterangepicker.js"></script>
-	<!-- bootstrap color picker -->
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
-	<!-- Tempusdominus Bootstrap 4 -->
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-	<!-- Bootstrap Switch -->
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
-	<!-- BS-Stepper -->
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/bs-stepper/js/bs-stepper.min.js"></script>
-
+	<!-- DataTables  & Plugins -->
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables/jquery.dataTables.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/jszip/jszip.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/pdfmake/pdfmake.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/pdfmake/vfs_fonts.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 	<!-- AdminLTE App -->
 	<script src="<?php echo APP_URL; ?>app/views/dist/js/adminlte.min.js"></script>
-		
-	<script src="<?php echo APP_URL; ?>app/views/dist/js/ajax.js" ></script>
-
 	<script src="<?php echo APP_URL; ?>app/views/dist/js/sweetalert2.all.min.js" ></script>
-
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/jquery-validation/jquery.validate.min.js"></script>
-	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/jquery-validation/additional-methods.min.js"></script>
 	
+	<script src="<?php echo APP_URL; ?>app/views/dist/js/ajax.js" ></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/js/main.js" ></script>
+	
+	<!-- Page specific script -->
 	<script>
 		$(function () {
-
-			//Datemask dd/mm/yyyy
-			$('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
-			//Datemask2 mm/dd/yyyy
-			$('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
-			//Money Euro
-			$('[data-mask]').inputmask()
-
-		})  
-	</script>	
-
-	<script>
-		$(function () {
-			
-			$('#quickForm').validate({
-				rules: {
-				nombre_tabla: {
-					required: true       
-				},
-				},
-				messages: {
-				nombre_tabla: {
-					required: "Por favor ingrese un nombre de tabla"
-				},
-				},
-				errorElement: 'span',
-				errorPlacement: function (error, element) {
-				error.addClass('invalid-feedback');
-				element.closest('.form-group').append(error);
-				},
-				highlight: function (element, errorClass, validClass) {
-				$(element).addClass('is-invalid');
-				},
-				unhighlight: function (element, errorClass, validClass) {
-				$(element).removeClass('is-invalid');
-				}
-			});
+			$("#example1").DataTable({
+			"responsive": true, "lengthChange": false, "autoWidth": false,
+			}).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');			    
 		});
-	</script>
+	</script>    
   </body>
 </html>
+
+
+
+
+
+
+
+
