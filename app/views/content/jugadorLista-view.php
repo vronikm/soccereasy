fe06560c1f6189a3116e63860c0b0cd4b9ad3a39
fbpinzon <fbpinzon@gmail.com>
2024-08-04 12:26:07 -0500

    ajuste modulo torneos

---
+++ app/views/content/jugadorLista-view.php
@@ -0,0 +1,176 @@
+<?php
+	use app\controllers\jugadorController;
+	$insUsuario = new jugadorController();	
+
+	$equipo_id 		= ($url[1] != "") ? $url[1] : 0;
+
+	if($equipo_id != 0){
+		$nombreEquipo=$insJugador->BuscarEquipo($equipo_id);		
+		if($nombreEquipo->rowCount()==1){
+			$nombreEquipo	=	$nombreEquipo->fetch();				
+			$equipo_nombre	= 	$nombreEquipo['equipo_nombre'];		
+		}
+	}else{
+		$equipo_nombre = '';
+	}
+
+	
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo APP_NAME; ?>| Jugadores </title>
+
+	<!-- Google Font: Source Sans Pro -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/fontawesome-free/css/all.min.css">
+	<!-- DataTables -->
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/css/adminlte.min.css">
+
+
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/dist/css/sweetalert2.min.css">
+	<script src="<?php echo APP_URL; ?>app/views/dist/js/sweetalert2.all.min.js" ></script>
+    
+  </head>
+  <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+      <!-- Preloader -->
+      <!--?php require_once "app/views/inc/preloader.php"; ?-->
+      <!-- /.Preloader -->
+
+      <!-- Navbar -->
+      <?php require_once "app/views/inc/navbar.php"; ?>
+      <!-- /.navbar -->
+
+      <!-- Main Sidebar Container -->
+      <?php require_once "app/views/inc/main-sidebar.php"; ?>
+      <!-- /.Main Sidebar Container -->  
+
+      <!-- vista -->
+      <div class="content-wrapper">
+
+		<!-- Content Header (Page header) -->
+		<div class="content-header">
+			<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+				<h1 class="m-0">Usuarios</h1>
+				</div><!-- /.col -->
+				<div class="col-sm-6">
+				<ol class="breadcrumb float-sm-right">
+					<li class="breadcrumb-item"><a href="#">Nuevo</a></li>
+					<li class="breadcrumb-item active">Dashboard v1</li>
+				</ol>
+				</div><!-- /.col -->
+			</div><!-- /.row -->
+			</div><!-- /.container-fluid -->
+		</div>
+		<!-- /.content-header -->
+
+		<!-- Main content -->
+		<section class="content">
+			<div class="container-fluid">
+			<!-- Small boxes (Stat box) -->
+				<div class="card card-default">
+					<div class="card-header">
+						<h3 class="card-title">Listado de usuarios</h3>
+						<div class="card-tools">
+							<a href="<?php echo APP_URL; ?>userNew/" class="btn btn-primary btn-sm" >Nuevo Usuario</a>
+							<button type="button" class="btn btn-tool" data-card-widget="collapse">
+								<i class="fas fa-minus"></i>
+							</button>
+						</div>
+					</div>
+
+					<div class="card-body">
+						<table id="example1" class="table table-bordered table-striped table-sm">
+							<thead>
+								<tr>
+									<th>Usuario</th>
+									<th>Nombre</th>
+									<th>Rol</th>
+									<th>Creación</th>
+									<th>Modificación</th>
+									<th>Estado</th>
+									<th style="width: 220px;">Opciones</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php 
+									echo $insUsuario->listarUsuarios(); 
+								?>							
+							</tbody>	
+						</table>	
+					</div>
+				</div>
+			<!-- /.row -->
+			</div><!-- /.container-fluid -->
+		</section>
+		<!-- /.content -->
+      
+      </div>
+      <!-- /.vista -->
+
+      <?php require_once "app/views/inc/footer.php"; ?>
+
+      <!-- Control Sidebar -->
+      <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+      </aside>
+      <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    
+	<!-- jQuery -->
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/jquery/jquery.min.js"></script>
+	<!-- Bootstrap 4 -->
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- DataTables  & Plugins -->
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables/jquery.dataTables.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/jszip/jszip.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/pdfmake/pdfmake.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/pdfmake/vfs_fonts.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+	<!-- AdminLTE App -->
+	<script src="<?php echo APP_URL; ?>app/views/dist/js/adminlte.min.js"></script>
+	
+	<!-- Page specific script -->
+	<script>
+		$(function () {
+			$("#example1").DataTable({
+			"responsive": true, "lengthChange": false, "autoWidth": false,
+			}).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');			    
+		});
+	</script>
+
+	<script src="<?php echo APP_URL; ?>app/views/dist/js/ajax.js" ></script>
+	<script src="<?php echo APP_URL; ?>app/views/dist/js/main.js" ></script>
+    
+  </body>
+</html>
+
+
+
+
+
+
+
+
