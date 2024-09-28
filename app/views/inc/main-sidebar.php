786e2c214d17c24e9ac22e5e0ebfe48b7200f0fd
vronik <veronicaquinde@digitech.com.ec>
2024-09-27 22:17:33 -0500

    Modificar menu por nuevo reporte

--- app/views/inc/main-sidebar.php
+++ app/views/inc/main-sidebar.php
@@ -171,6 +171,15 @@
                 </a>
               </li>
             </ul>
+
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="<?php echo APP_URL."reportePagosReceptadosResumen/" ?>" class="nav-link">
+                  <i class="nav-icon far fa-circle text-info"></i>
+                  <p>Resumen pagos</p>
+                </a>
+              </li>
+            </ul>
           </li>
 
           <li class="nav-header">Seguridad</li>
