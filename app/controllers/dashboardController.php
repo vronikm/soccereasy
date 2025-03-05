70599922a451d19e35b6c137d74fa5a2db206829
vronik <veronicaquinde@digitech.com.ec>
2025-03-04 22:53:25 -0500

    Presentar en dashboard los alumnos activos e inactivos

--- app/controllers/dashboardController.php
+++ app/controllers/dashboardController.php
@@ -102,7 +102,8 @@
 																			pago_alumnoid
 																	) BASE
 																) PEN ON PEN.pago_alumnoid = A.alumno_id
-																WHERE PEN.TOTAL > 0 OR P.SALDO > 0 
+																WHERE A.alumno_estado <> 'E'
+																	AND PEN.TOTAL > 0 OR P.SALDO > 0 
 															) AS subconsulta;");
 			return $pagosPendienteSedeL;
 		}
